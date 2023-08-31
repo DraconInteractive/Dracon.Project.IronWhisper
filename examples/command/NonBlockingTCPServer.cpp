4d0a3cd33b8cf292fe6da9de5bf2cf98160e77e7
Peter Carey <petermarkcarey@gmail.com>
2023-08-31 11:29:59 +0800

    working socket, remove command-file mode

--- examples/command/NonBlockingTCPServer.cpp
+++ examples/command/NonBlockingTCPServer.cpp
@@ -8,97 +8,177 @@
 #include <fcntl.h>
 #include <arpa/inet.h>
 
-NonBlockingTCPServer::NonBlockingTCPServer() : serverSocket(0), maxFd(0), lastSendTime(time(nullptr)) {}
+NonBlockingTCPServer::NonBlockingTCPServer() : serverSocket(0), maxFd(0), lastSendTime(time(nullptr)) 
+{
+    FD_ZERO(&readFds);
+}
+
+void printPeerInfo(int clientSocket) {
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddressLen = sizeof(clientAddress);
+
+    if (getpeername(clientSocket, (struct sockaddr*)&clientAddress, &clientAddressLen) == 0) {
+        char clientIP[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientAddress.sin_addr, clientIP, sizeof(clientIP));
+        printf("  IP: %s,\n  Port: %d\n", clientIP, ntohs(clientAddress.sin_port));
+    } else {
+        perror("getpeername");
+    }
+}
+
+void checkSocketState(int clientSocket)
+{
+    int socketError = 0;
+    socklen_t socketErrorLen = sizeof(socketError);
+
+    if (getsockopt(clientSocket, SOL_SOCKET, SO_ERROR, &socketError, &socketErrorLen) == 0) {
+        if (socketError == 0) {
+            printf("Socket %d is open and error-free\n", clientSocket);
+        } else {
+            printf("Socket #%d error: %s\n", clientSocket, strerror(socketError));
+        }
+    } else {
+        perror("getsockopt");
+        // Handle the error
+    }
+}
 
 void NonBlockingTCPServer::setupServer(int port) {
+    // Create the server socket
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Configure the server address
     struct sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(port);
     serverAddress.sin_addr.s_addr = inet_addr("172.28.31.175");
 
+    // Bind the server socket to the specified address and port
     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
+    // Listen for incoming connections
     listen(serverSocket, 5);
     fprintf(stdout, "[socket] server initialized, bound and listening\n");
 } 
 
 void NonBlockingTCPServer::acceptConnections() {
+    fprintf(stdout, "[socket] accepting connections\n");
 
-    int error = 0;
-    socklen_t errorLen = sizeof(error);
-
-    if (getsockopt(serverSocket, SOL_SOCKET, SO_ERROR, &error, &errorLen) != 0)
-    {
-        fprintf(stdout, "getsockopt error\n");
-    }
-    if (error == 0)
-    {
-        fprintf(stdout, "socket is open\n");
-    }
-    else {
-        fprintf(stdout, "socket is closed/errored\n");
-        fprintf(stdout, "socket error: %s\n", strerror(error));
-    }
-    fprintf(stdout, "[socket] checking for connections... \n");
-    fd_set readFds;
     FD_ZERO(&readFds);
+
+    // Add server socket to the set
     FD_SET(serverSocket, &readFds);
 
+    fprintf(stdout, "[socket] assessing %zu clients...\n", clientSockets.size());
+    fflush(stdout);
+
+    // Find the maximum file descriptor in the set
     maxFd = serverSocket;
 
+    // Iterate through each client socket in the collection
     for (int clientSocket : clientSockets) {
-        FD_SET(clientSocket, &readFds);
-        maxFd = std::max(maxFd, clientSocket);
+        fprintf(stdout, "%d: \n", clientSocket);
+        // Check if valid and connected
+        if (clientSocket >= 0)
+        {
+            FD_SET(clientSocket, &readFds);
+            maxFd = std::max(maxFd, clientSocket);
+            printPeerInfo(clientSocket);
+            checkSocketState(clientSocket);
+        }
     }
 
+    // Set up timeout for select
     struct timeval timeout;
-    timeout.tv_sec = 2;
-    timeout.tv_usec = 0;
-    //int activity = select(maxFd + 1, &readFds, nullptr, nullptr, &timeout);
-    int activity = select(maxFd + 1, &readFds, nullptr, nullptr, nullptr);
-    fprintf(stdout, "[socket] select activity: %d\n", activity);
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 250;
+
+    // Perform select to monitor file descriptors for activity
+    int activity = select(maxFd + 1, &readFds, nullptr, nullptr, &timeout);
+
+    // Check for errors
     if (activity == -1) {
         perror("select");
         return;
     }
 
-    if (FD_ISSET(serverSocket, &readFds)) {
-        fprintf(stdout, "[socket] Client found, ready for reading\n");
-        int clientSocket = accept(serverSocket, nullptr, nullptr);
-        fcntl(clientSocket, F_SETFL, O_NONBLOCK);
-        clientSockets.insert(clientSocket);
-    }
-    else 
+    if (activity >= 1)
     {
-        fprintf(stdout, "[socket] no clients found\n");
+        // Check for new connections
+        if (FD_ISSET(serverSocket, &readFds)) {
+            fprintf(stdout, "[socket] Client found, ready for reading\n");
+
+            // Accept a new client connection
+            int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+            // Set the client socket to non-blocking mode
+            int blockResult = fcntl(clientSocket, F_SETFL, O_NONBLOCK);
+            if (blockResult == -1)
+            {
+                perror("fcntl");
+            }
+            // Add the new client socket to the collection
+            clientSockets.insert(clientSocket);
+        }
+        else 
+        {
+            fprintf(stdout, "[socket] no clients found\n");
+        }
     }
+    else {
+        fprintf(stdout, "[socket] no client activity\n");
+    }
+
+    
 }
 
 void NonBlockingTCPServer::sendDataToClients(const char* messageToSend) {
+    // Check if the message is valid
     if (messageToSend == nullptr || *messageToSend == '\0')
     {
         fprintf(stderr, "[socket] null or empty c_str provided, not sending data\n");
         return;
     }
-    fd_set readFds;
+
+    // Iterate through each client socket in the collection
     for (int clientSocket : clientSockets) {
-        send(clientSocket, messageToSend, strlen(messageToSend), 0);
+        // Send the message to the client
+        ssize_t bytesSent = send(clientSocket, messageToSend, strlen(messageToSend), 0);
+        fprintf(stdout, "sent %zd bytes\n", bytesSent);
+        if (bytesSent == -1)
+        {
+            fprintf(stderr, "[socket] error sending packet to client: %d\n", clientSocket);
+            perror("send");
+        }
     }
-    fprintf(stdout, "[socket] data sent\n");
 }
 
 void NonBlockingTCPServer::receiveDataFromClients() {
-    fd_set readFds;
+    // Iterate through each client socket in the collection
     for (int clientSocket : clientSockets) {
+        // Check if the socket has data to be read
         if (FD_ISSET(clientSocket, &readFds)) {
+            // Buffer to store received data
             char buffer[1024];
+
+            // Receive data from the client
             ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-            if (bytesReceived <= 0) {
+
+            // Check if there's an issue with the connection
+            if (bytesReceived == 0) {
+                // Close the client socket and remove it from the collection
                 close(clientSocket);
                 clientSockets.erase(clientSocket);
-            } else {
+            }
+            else if (bytesReceived < 0)
+            {
+                perror("recv");
+                close(clientSocket);
+                clientSockets.erase(clientSocket);
+            } 
+            else 
+            {
+                // Null-terminate the received data and print it
                 buffer[bytesReceived] = '\0';
                 std::cout << "Received from client " << clientSocket << ": " << buffer << std::endl;
             }
@@ -107,8 +187,12 @@ void NonBlockingTCPServer::receiveDataFromClients() {
 }
 
 void NonBlockingTCPServer::closeServer() {
+    // Iterate through each client socket in the collection
     for (int clientSocket : clientSockets) {
+        // Close each client socket
         close(clientSocket);
     }
+
+    // Close the server socket
     close(serverSocket);
 }
--- examples/command/command.cpp
+++ examples/command/command.cpp
@@ -15,7 +15,7 @@
 #include <sstream>
 #include <cassert>
 #include <cstdio>
-#include <fstream>
+#include <fstream> 
 #include <mutex>
 #include <regex>
 #include <string>
@@ -44,7 +44,6 @@ struct whisper_params {
     std::string language  = "en";
     std::string model     = "models/ggml-base.en.bin";
     std::string fname_out;
-    std::string commands;
     std::string prompt;
     std::string open_gate;
     std::string close_gate;
@@ -57,33 +56,26 @@ std::string lastSent;
 void whisper_print_usage(int argc, char ** argv, const whisper_params & params);
 
 void socket_tick() {
-    fprintf(stdout, "Accepting socket connections...\n");
     server.acceptConnections();
-    fprintf(stdout, "Checking for new data to send...\n");
-    fflush(stdout);
     // This function only gets called when something is heard, this is just making sure
-    if (!lastHeard.empty() && lastHeard != lastSent)
+    if (!lastHeard.empty() && lastHeard != lastSent) 
     {
-        fprintf(stdout, "Sending data...\n");
         lastSent = lastHeard;
         server.sendDataToClients(lastHeard.c_str());
     }
-    fprintf(stdout, "Receiving data...\n");
 
-    server.receiveDataFromClients();
-    fprintf(stdout, "Server tick complete\n");
-    fflush(stdout);
+    server.receiveDataFromClients(); 
 }
 
 bool whisper_params_parse(int argc, char ** argv, whisper_params & params) {
     for (int i = 1; i < argc; i++) {
-        std::string arg = argv[i];
+        std::string arg = argv[i]; 
 
         if (arg == "-h" || arg == "--help") {
             whisper_print_usage(argc, argv, params);
             exit(0);
         }
-        else if (arg == "-t"   || arg == "--threads")       { params.n_threads     = std::stoi(argv[++i]); }
+        else if (arg == "-t"   || arg == "--threads")       { params.n_threads     = std::stoi(argv[++i]); } 
         else if (arg == "-pms" || arg == "--prompt-ms")     { params.prompt_ms     = std::stoi(argv[++i]); }
         else if (arg == "-cms" || arg == "--command-ms")    { params.command_ms    = std::stoi(argv[++i]); }
         else if (arg == "-c"   || arg == "--capture")       { params.capture_id    = std::stoi(argv[++i]); }
@@ -98,7 +90,6 @@ bool whisper_params_parse(int argc, char ** argv, whisper_params & params) {
         else if (arg == "-l"   || arg == "--language")      { params.language      = argv[++i]; }
         else if (arg == "-m"   || arg == "--model")         { params.model         = argv[++i]; }
         else if (arg == "-f"   || arg == "--file")          { params.fname_out     = argv[++i]; }
-        else if (arg == "-cmd" || arg == "--commands")      { params.commands      = argv[++i]; }
         else if (arg == "-p"   || arg == "--prompt")        { params.prompt        = argv[++i]; }
         else if (arg == "-og"  || arg == "--open-gate")    { params.open_gate     = argv[i++]; }
         else if (arg == "-cg"  || arg == "--close-gate")   { params.close_gate    = argv[i++]; }
@@ -106,11 +97,11 @@ bool whisper_params_parse(int argc, char ** argv, whisper_params & params) {
             fprintf(stderr, "error: unknown argument: %s\n", arg.c_str());
             whisper_print_usage(argc, argv, params);
             exit(0);
-        }
+        } 
     }
 
     return true;
-}
+} 
 
 void whisper_print_usage(int /*argc*/, char ** argv, const whisper_params & params) {
     fprintf(stderr, "\n");
@@ -133,8 +124,9 @@ void whisper_print_usage(int /*argc*/, char ** argv, const whisper_params & para
     fprintf(stderr, "  -l LANG,    --language LANG  [%-7s] spoken language\n",                             params.language.c_str());
     fprintf(stderr, "  -m FNAME,   --model FNAME    [%-7s] model path\n",                                  params.model.c_str());
     fprintf(stderr, "  -f FNAME,   --file FNAME     [%-7s] text output file name\n",                       params.fname_out.c_str());
-    fprintf(stderr, "  -cmd FNAME, --commands FNAME [%-7s] text file with allowed commands\n",             params.commands.c_str());
-    fprintf(stderr, "  -p,         --prompt         [%-7s] the required activation prompt\n",              params.prompt.c_str());
+    fprintf(stderr, "  -p,         --prompt         [%-7s] single use activation prompt\n"  ,              params.prompt.c_str());
+    fprintf(stderr, "  -og,        --open-gate      [%-7s] enter command mode prompt\n",                   params.open_gate.c_str());
+    fprintf(stderr, "  cd,         --close-gate     [%-7s] exit command mode prompt\n",                    params.close_gate.c_str());
     fprintf(stderr, "\n");
 }
 
@@ -192,28 +184,6 @@ std::string transcribe(whisper_context * ctx, const whisper_params & params, con
     return result;
 }
 
-std::vector<std::string> read_allowed_commands(const std::string & fname) {
-    std::vector<std::string> allowed_commands;
-
-    std::ifstream ifs(fname);
-    if (!ifs.is_open()) {
-        return allowed_commands;
-    }
-
-    std::string line;
-    while (std::getline(ifs, line)) {
-        line = ::trim(line);
-        if (line.empty()) {
-            continue;
-        }
-
-        std::transform(line.begin(), line.end(),line.begin(), ::tolower);
-        allowed_commands.push_back(std::move(line));
-    }
-
-    return allowed_commands;
-}
-
 std::vector<std::string> get_words(const std::string &txt) {
     std::vector<std::string> words;
 
@@ -224,218 +194,6 @@ std::vector<std::string> get_words(const std::string &txt) {
     }
 
     return words;
-}
-
-// command-list mode
-// guide the transcription to match the most likely command from a provided list
-int process_command_list(struct whisper_context * ctx, audio_async &audio, const whisper_params &params) {
-    fprintf(stderr, "\n");
-    fprintf(stderr, "%s: guided mode\n", __func__);
-
-    std::vector<std::string> allowed_commands = read_allowed_commands(params.commands);
-
-    if (allowed_commands.empty()) {
-        fprintf(stderr, "%s: error: failed to read allowed commands from '%s'\n", __func__, params.commands.c_str());
-        return 2;
-    }
-
-    int max_len = 0;
-
-    std::vector<std::vector<whisper_token>> allowed_tokens;
-
-    for (const auto & cmd : allowed_commands) {
-        whisper_token tokens[1024];
-        allowed_tokens.emplace_back();
-
-        for (int l = 0; l < (int) cmd.size(); ++l) {
-            // NOTE: very important to add the whitespace !
-            //       the reason is that the first decoded token starts with a whitespace too!
-            std::string ss = std::string(" ") + cmd.substr(0, l + 1);
-
-            const int n = whisper_tokenize(ctx, ss.c_str(), tokens, 1024);
-            if (n < 0) {
-                fprintf(stderr, "%s: error: failed to tokenize command '%s'\n", __func__, cmd.c_str());
-                return 3;
-            }
-
-            if (n == 1) {
-                allowed_tokens.back().push_back(tokens[0]);
-            }
-        }
-
-        max_len = std::max(max_len, (int) cmd.size());
-    }
-
-    fprintf(stderr, "%s: allowed commands [ tokens ]:\n", __func__);
-    fprintf(stderr, "\n");
-    for (int i = 0; i < (int) allowed_commands.size(); ++i) {
-        fprintf(stderr, "  - \033[1m%-*s\033[0m = [", max_len, allowed_commands[i].c_str());
-        for (const auto & token : allowed_tokens[i]) {
-            fprintf(stderr, " %5d", token);
-        }
-        fprintf(stderr, " ]\n");
-    }
-
-    std::string  k_prompt = "select one from the available words: ";
-    for (int i = 0; i < (int) allowed_commands.size(); ++i) {
-        if (i > 0) {
-            k_prompt += ", ";
-        }
-        k_prompt += allowed_commands[i];
-    }
-    k_prompt += ". selected word: ";
-
-    // tokenize prompt
-    std::vector<whisper_token> k_tokens;
-    {
-        k_tokens.resize(1024);
-        const int n = whisper_tokenize(ctx, k_prompt.c_str(), k_tokens.data(), 1024);
-        if (n < 0) {
-            fprintf(stderr, "%s: error: failed to tokenize prompt '%s'\n", __func__, k_prompt.c_str());
-            return 4;
-        }
-        k_tokens.resize(n);
-    }
-
-    fprintf(stderr, "\n");
-    fprintf(stderr, "%s: prompt: '%s'\n", __func__, k_prompt.c_str());
-    fprintf(stderr, "%s: tokens: [", __func__);
-    for (const auto & token : k_tokens) {
-        fprintf(stderr, " %d", token);
-    }
-    fprintf(stderr, " ]\n");
-
-    fprintf(stderr, "\n");
-    fprintf(stderr, "%s: listening for a command ...\n", __func__);
-    fprintf(stderr, "\n");
-
-    bool is_running  = true;
-
-    std::vector<float> pcmf32_cur;
-    std::vector<float> pcmf32_prompt;
-
-    // main loop
-    while (is_running) {
-        // handle Ctrl + C
-        is_running = sdl_poll_events();
-
-        // delay
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        audio.get(2000, pcmf32_cur);
-
-        if (::vad_simple(pcmf32_cur, WHISPER_SAMPLE_RATE, 1000, params.vad_thold, params.freq_thold, params.print_energy)) {
-            fprintf(stdout, "%s: Speech detected! Processing ...\n", __func__);
-
-            const auto t_start = std::chrono::high_resolution_clock::now();
-
-            whisper_full_params wparams = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
-
-            wparams.print_progress   = false;
-            wparams.print_special    = params.print_special;
-            wparams.print_realtime   = false;
-            wparams.print_timestamps = !params.no_timestamps;
-            wparams.translate        = params.translate;
-            wparams.no_context       = true;
-            wparams.single_segment   = true;
-            wparams.max_tokens       = 1;
-            wparams.language         = params.language.c_str();
-            wparams.n_threads        = params.n_threads;
-
-            wparams.audio_ctx        = params.audio_ctx;
-            wparams.speed_up         = params.speed_up;
-
-            wparams.prompt_tokens    = k_tokens.data();
-            wparams.prompt_n_tokens  = k_tokens.size();
-
-            // run the transformer and a single decoding pass
-            if (whisper_full(ctx, wparams, pcmf32_cur.data(), pcmf32_cur.size()) != 0) {
-                fprintf(stderr, "%s: ERROR: whisper_full() failed\n", __func__);
-                break;
-            }
-
-            // estimate command probability
-            // NOTE: not optimal
-            {
-                const auto * logits = whisper_get_logits(ctx);
-
-                std::vector<float> probs(whisper_n_vocab(ctx), 0.0f);
-
-                // compute probs from logits via softmax
-                {
-                    float max = -1e9;
-                    for (int i = 0; i < (int) probs.size(); ++i) {
-                        max = std::max(max, logits[i]);
-                    }
-
-                    float sum = 0.0f;
-                    for (int i = 0; i < (int) probs.size(); ++i) {
-                        probs[i] = expf(logits[i] - max);
-                        sum += probs[i];
-                    }
-
-                    for (int i = 0; i < (int) probs.size(); ++i) {
-                        probs[i] /= sum;
-                    }
-                }
-
-                std::vector<std::pair<float, int>> probs_id;
-
-                double psum = 0.0;
-                for (int i = 0; i < (int) allowed_commands.size(); ++i) {
-                    probs_id.emplace_back(probs[allowed_tokens[i][0]], i);
-                    for (int j = 1; j < (int) allowed_tokens[i].size(); ++j) {
-                        probs_id.back().first += probs[allowed_tokens[i][j]];
-                    }
-                    probs_id.back().first /= allowed_tokens[i].size();
-                    psum += probs_id.back().first;
-                }
-
-                // normalize
-                for (auto & p : probs_id) {
-                    p.first /= psum;
-                }
-
-                // sort descending
-                {
-                    using pair_type = decltype(probs_id)::value_type;
-                    std::sort(probs_id.begin(), probs_id.end(), [](const pair_type & a, const pair_type & b) {
-                        return a.first > b.first;
-                    });
-                }
-
-                // print the commands and the respective probabilities
-                {
-                    fprintf(stdout, "\n");
-                    for (const auto & cmd : probs_id) {
-                        fprintf(stdout, "%s: %s%-*s%s = %f | ", __func__, "\033[1m", max_len, allowed_commands[cmd.second].c_str(), "\033[0m", cmd.first);
-                        for (int token : allowed_tokens[cmd.second]) {
-                            fprintf(stdout, "'%4s' %f ", whisper_token_to_str(ctx, token), probs[token]);
-                        }
-                        fprintf(stdout, "\n");
-                    }
-                }
-
-                // best command
-                {
-                    const auto t_end = std::chrono::high_resolution_clock::now();
-
-                    const float prob = probs_id[0].first;
-                    const int index = probs_id[0].second;
-
-                    fprintf(stdout, "\n");
-                    fprintf(stdout, "%s: detected command: %s%s%s | p = %f | t = %d ms\n", __func__,
-                            "\033[1m", allowed_commands[index].c_str(), "\033[0m", prob,
-                            (int) std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count());
-                    fprintf(stdout, "\n");
-                }
-            }
-
-            audio.clear();
-        }
-    }
-
-    return 0;
 }
 
 // always-prompt mode
@@ -699,7 +457,8 @@ int main(int argc, char ** argv) {
     // socket init  
     server.setupServer(31050); 
     fprintf(stderr, "[socket] communication opened. Port: 31050\n");
-    // whisper init  
+    
+    // whisper init   
 
     struct whisper_context * ctx = whisper_init_from_file(params.model.c_str());
 
@@ -717,7 +476,7 @@ int main(int argc, char ** argv) {
                 __func__,
                 params.n_threads,
                 params.language.c_str(),
-                params.translate ? "translate" : "transcribe",
+                params.translate ? "translate" : "transcribe", 
                 params.no_timestamps ? 0 : 1);
 
         fprintf(stderr, "\n");
@@ -739,9 +498,7 @@ int main(int argc, char ** argv) {
 
     int  ret_val = 0;
 
-    if (!params.commands.empty()) {
-        ret_val = process_command_list(ctx, audio, params);
-    } else if (!params.prompt.empty()) {
+    if (!params.prompt.empty()) {
         ret_val = always_prompt_transcription(ctx, audio, params);
     } else if (!params.open_gate.empty() && !params.close_gate.empty()) {
         ret_val = process_gated_transcription(ctx, audio, params);
